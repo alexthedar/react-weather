615bcbcb725859c256b0c79c2a891510f2d3888f
alexthedar <alex.castaneda@gmail.com>
2017-02-22 11:07:44 -0800

    logic for modal but still cannot do search after failure

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -7,15 +7,14 @@ module.exports = {
   getTemp: function(location){
     var encodedLocation = encodeURIComponent(location)
     var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`
-console.log(requestUrl)
     return axios.get(requestUrl).then(function(res){
-      if(res.data.code && res.data.message){
+      if(res.data.cod && res.data.message){
         throw new Error(res.data.message)
       } else {
         return res.data.main.temp
       }
-    }, function(res){
-      throw new Error(res.data.message)
+    }, function(err){
+      throw new Error(err.data.message);
     })
   }
 }
--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,16 +1,29 @@
 var React = require('react')
 
 var ErrorModal = React.createClass({
+  getDefaultProps: function(){
+    return {
+      title: 'Error'
+    }
+  },
+  proprTypes: {
+    // title: React.PropTypes.string,
+    // message: React.Proptypes.string.isRequired
+  },
   componentDidMount: function (){
     var modal = new Foundation.Reveal($('#error-modal'))
     modal.open()
   },
   render: function(){
-    <div id="error-modal" className="reveal tiny text-center" data-reveal=''>
-      <h4>Tite</h4>
-      <p>erro message</p>
-      <p><button className="button hollow" data-close=''>OK</button></p>
-    </div>
+    var {title, message} = this.props
+
+    return (
+      <div id="error-modal" className="reveal tiny text-center" data-reveal=''>
+        <h4>{title}</h4>
+        <p>{message}</p>
+        <p><button className="button hollow" data-close=''>OK</button></p>
+      </div>
+    )
   }
 })
 
